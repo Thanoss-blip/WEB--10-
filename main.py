7c0b91eeba631921587580d0b9aa7a73d845ca80
Thanoss-blip <74791792+Thanoss-blip@users.noreply.github.com>
2023-02-21 01:43:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -182,6 +182,6 @@ def logout():
 
 if __name__ == '__main__':
     db_session.global_init("db/blogs.db")
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT"))
     app.run(host='0.0.0.0', port=port, debug=True)
 
